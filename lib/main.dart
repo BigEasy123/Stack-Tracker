6fdb6714eeb1974981fd1723643ee64aa5a2403f
Ben White <mwhiteben@gmail.com>
2025-08-07 00:35:30 -0400

    Add purchase deletion and realized P/L features

--- lib/main.dart
+++ lib/main.dart
@@ -210,8 +210,8 @@ class _InvestmentFormPageState extends State<InvestmentFormPage> {
 
   Future<void> _fetchPrices() async {
     setState(() => _isLoading = true);
-
-    final url = Uri.parse('https://metal-price-api-wf1l.onrender.com');
+    const String apiBaseUrl = 'https://metal-price-api-wf1l.onrender.com';
+    final url = Uri.parse('$apiBaseUrl/prices');
 
     try {
       final response = await http.get(url);
